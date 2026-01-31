7cb15d9a52384590bb4838f4edd38d6d60d32231
ExperienciaUTN <experienciautn@gmail.com>
2026-01-31 20:35:12 +0000

    cambios revertidos

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -2,10 +2,5 @@
 
 int main(int argc, char* argv[]) {
     saludar("kernel");
-    int cambio;
-    int cambio2;
-    int cambio3 = 1;
-    char* cambio5 = "hola";
-    int cambio4 = 3;
     return 0;
 }
